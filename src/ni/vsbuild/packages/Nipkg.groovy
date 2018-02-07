e89587e5299d8a3134f554e39eb5f388c3832ab3
Andy Church <andy.church@ni.com>
2018-02-07 17:25:31 -0600

    added stagingPath and robocopy to move payload

--- src/ni/vsbuild/packages/Nipkg.groovy
+++ src/ni/vsbuild/packages/Nipkg.groovy
@@ -3,20 +3,24 @@ package ni.vsbuild.packages
 class Nipkg extends AbstractPackage {
 
    def releaseVersion
+   def stagingPath
    
    Nipkg(script, packageInfo, payloadDir) {
       super(script, packageInfo, payloadDir)
       this.releaseVersion = packageInfo.get('release_version')
+      this.stagingPath = packageInfo.get('install_destination')
    }
 
    void buildPackage(lvVersion) {
       def packageInfo = """
          Building package $name from $payloadDir
          Package version: $releaseVersion
+         Staging path: $stagingPath
          LabVIEW/VeriStand version: $lvVersion
          """.stripIndent()
       
-      script.buildNipkg(payloadDir, releaseVersion, lvVersion)
+      script.buildNipkg(payloadDir, releaseVersion, stagingPath, lvVersion)
       script.echo packageInfo
    }
 }
+
--- vars/buildNipkg.groovy
+++ vars/buildNipkg.groovy
@@ -1,13 +1,20 @@
-def call(releaseVersion, payloadDir, lvVersion) {
+def call(releaseVersion, payloadDir, stagingPath, lvVersion) {
    
    def controlFields = readProperties file: "control"
+   def packageName = "${controlFields.get('Package')}-${lvVersion}"
+   echo "Building ${packageName} with control attributes:"
    echo controlFields.toMapString()
 
    def controlFileText = readFile "control"
    echo controlFileText
 
    def newControlFileText = controlFileText.replaceAll("\\{version\\}", "${lvVersion}")
+   
+   def newStagingPath = stagingPath.replaceAll("\\{version\\}", "${lvVersion}")
+   echo "Staging path: $newStagingPath"
 
+   bat "(robocopy \"${payloadDir}\" \"${newStagingPath}\" /MIR /NFL /NDL /NJH /NJS /nc /ns /np) ^& exit 0"
+      
    writeFile file: "nipkg\\control", text: newControlFileText
 
 }
