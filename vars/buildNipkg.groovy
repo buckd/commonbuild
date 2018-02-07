d43375fd8d260284ce8ffac9463abe314fa291a8
Andy Church <andy.church@ni.com>
2018-02-07 15:23:48 -0600

    loading control file text as string

--- vars/buildNipkg.groovy
+++ vars/buildNipkg.groovy
@@ -4,4 +4,13 @@ def call(releaseVersion, payloadDir, lvVersion) {
    
    echo controlFields.toMapString()
 
+   def controlFileText = new File("control")
+
+   echo controlFileText
+
+   def newControlFileText = controlFileText
+   def newControlFile = new File("nipkg\\control")
+   
+   newControlFile.text = newControlFileText
+
 }
