1969bcbb37e63a919f88546ce334679894e6b023
Andy Church <andy.church@ni.com>
2018-02-07 16:50:26 -0600

    added replaceAll() for {version} expression

--- vars/buildNipkg.groovy
+++ vars/buildNipkg.groovy
@@ -1,15 +1,13 @@
 def call(releaseVersion, payloadDir, lvVersion) {
    
    def controlFields = readProperties file: "control"
-   
    echo controlFields.toMapString()
 
    def controlFileText = readFile "control"
-
    echo controlFileText
 
-   def newControlFileText = controlFileText
-   
+   def newControlFileText = controlFileText.replaceAll("{version}", "${lvVersion}")
+
    writeFile file: "nipkg\\control", text: newControlFileText
 
 }
