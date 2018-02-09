b2256825a7d1878a1aeb78d39c036e7d8a7576cd
Andy Church <andy.church@ni.com>
2018-02-08 18:40:10 -0600

    changed nipkg destination

--- vars/buildNipkg.groovy
+++ vars/buildNipkg.groovy
@@ -5,23 +5,25 @@ def call(payloadDir, releaseVersion, stagingPath, lvVersion) {
    def packageName = basePackageName.replaceAll("\\{version\\}", "${lvVersion}")
    def nipmAppPath = "C:\\Program Files\\National Instruments\\NI Package Manager\\nipkg.exe"
    
-   echo "Building ${packageName} with control attributes:"
-   echo controlFields.toMapString()
+   echo "Building ${packageName} with control file attributes:"
 
-   def controlFileText = readFile "control"
+   def controlFileText = readFile "control" // Read nipkg control file 
    echo controlFileText
-
+   
+   // Replace {version} with current lvVersion.
    def newControlFileText = controlFileText.replaceAll("\\{version\\}", "${lvVersion}")
    
    def newStagingPath = stagingPath.replaceAll("\\{version\\}", "${lvVersion}")
    echo "Staging path: $newStagingPath"
 
+   // Copy package payload to nipkg staging directory. 
    bat "(robocopy \"${payloadDir}\" \"nipkg\\${packageName}\\data\\${newStagingPath}\" /MIR /NFL /NDL /NJH /NJS /nc /ns /np) ^& exit 0"
 
    writeFile file: "nipkg\\${packageName}\\debian-binary", text: "2.0"      
    writeFile file: "nipkg\\${packageName}\\control\\control", text: newControlFileText
 
-   bat "\"${nipmAppPath}\" pack nipkg\\${packageName} built\\nipkg" 
+   // Build nipkg using NI Package Manager CLI pack command. 
+   bat "\"${nipmAppPath}\" pack nipkg\\${packageName} ${payloadDir}" 
 
 }
 
