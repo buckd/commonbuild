4347ee4f13ff9c287fb957a360ed1429d5a6426f
Andy Church <andy.church@ni.com>
2018-09-26 23:20:17 -0500

    fix argument handling; use Popen

--- resources/addRemoveNipkg.py
+++ resources/addRemoveNipkg.py
@@ -3,18 +3,17 @@ import sys
 import subprocess
 
 operation = sys.argv[1]
-if len(sys.argv) > 1:
-	nipkgPath = sys.argv[2]
-
 currentDir = os.getcwd()
-nipmAppPath = os.environ["ProgramW6432"]+"\\National Instruments\\NI Package Manager\\nipkg.exe"
-nipkgRelPath = currentDir + "\\" + nipkgPath
+if len(sys.argv) > 2:
+	nipkgPath = sys.argv[2]
+	nipkgRelPath = currentDir + "\\" + nipkgPath
 
-print("Installing .nipkg file:\n", nipkgPath)
-print(f'\'{nipkgRelPath}\'')
+nipmAppPath = os.environ["ProgramW6432"]+"\\National Instruments\\NI Package Manager\\nipkg.exe"
 
 if operation == 'install' or operation == 'uninstall':
 	try:
+		print(operation, " .nipkg file:\n", nipkgPath)
+		print(f'\'{nipkgRelPath}\'')
 		subprocess.run([nipmAppPath, operation, "-y", nipkgRelPath], stderr=subprocess.STDOUT, shell=True, check=True)
 
 	except subprocess.CalledProcessError as err:
@@ -24,8 +23,8 @@ if operation == 'install' or operation == 'uninstall':
 if operation == 'list-installed':
 	file = open("nipm_version_manifest", "a")
 	try:
-		subprocess.run([nipmAppPath, operation], stderr=subprocess.STDOUT,  stdout=subprocess.PIPE, shell=True, check=True)
-		output = proc.stdout.read()
+		proc = subprocess.Popen([nipmAppPath, operation], stderr=subprocess.STDOUT,  stdout=subprocess.PIPE, shell=True)
+		output = proc.stdout.read().decode("utf-8")
 		file.write(output)
 
 	except subprocess.CalledProcessError as err:
