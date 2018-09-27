8cc62dbda57af0f39b74534dad48d7d6a91afe66
Andy Church <andy.church@ni.com>
2018-09-27 00:10:51 -0500

    remove VIPM bug workaround code from vipmGetInstalled; write nipm packages directly to build_log

--- resources/addRemoveNipkg.py
+++ resources/addRemoveNipkg.py
@@ -21,7 +21,8 @@ if operation == 'install' or operation == 'uninstall':
 		sys.exit(err.args[0])
 
 if operation == 'list-installed':
-	file = open("nipm_version_manifest", "a")
+	file = open("build_log", "a")
+	file.write("Installed NI Packages:\n\n")
 	try:
 		proc = subprocess.Popen([nipmAppPath, operation], stderr=subprocess.STDOUT,  stdout=subprocess.PIPE, shell=True)
 		output = proc.stdout.read().decode("utf-8")
--- vars/nipmGetInstalled.groovy
+++ vars/nipmGetInstalled.groovy
@@ -5,10 +5,4 @@ def call(){
    //Use NI Package Managet CLI to get list of installed NI Packages.
    bat """call python.exe commonbuild\\resources\\addRemoveNipkg.py list-installed
    exit %ERRORLEVEL%"""
-
-   def installedNipkgList = readFile "nipm_version_manifest"
-
-   //Add NIPM output to build log.
-   def buildLog = readFile 'build_log'
-   writeFile file: 'build_log', text: buildLog+"\r\n"+installedNipkgList
 }
